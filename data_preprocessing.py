3bdaea506ab2cadbd4faa0fc14c2f8507dd0c739
Teghbeer Singh <156863823+TEGHBEER@users.noreply.github.com>
2024-11-20 23:12:28 -0500

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,12 +1,13 @@
 from sklearn.datasets import load_breast_cancer
 import pandas as pd
 
-from sklearn.preprocessing import MinMaxScaler
-
 def load_and_preprocess_data():
+    # Load the dataset
     data = load_breast_cancer()
+    
+    # Convert to DataFrame for features and Series for target
     X = pd.DataFrame(data.data, columns=data.feature_names)
     y = pd.Series(data.target, name='target')
-    scaler = MinMaxScaler()  # MinMaxScaler scales data to a range of 0 to 1
-    X_scaled = scaler.fit_transform(X)
-    return X_scaled, y, scaler
+    
+    
+    return X, y
